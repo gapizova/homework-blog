f3ab469b7bec6f298436c624f255ba561cd64cea
gapizova <127729358+gapizova@users.noreply.github.com>
2023-10-20 07:39:40 +0000

    Deploying to gh-pages from @ gapizova/homework-blog@2399ab56469c22c56682bd1173adcf6bccb94585 🚀

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  plugins: {
-    // https://github.com/webpack-contrib/postcss-loader/blob/v3.0.0/README.md#configuration
-    'postcss-import': {},
-    autoprefixer: {},
-    'postcss-preset-env': {},
-    cssnano: {},
-  },
-};
--- src/index.js
+++
@@ -1,1 +0,0 @@
-import './styles/SCSS/style.scss';
